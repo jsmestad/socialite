50bb2de2925bf04a049d842c04ef05bf6dedee25
Justin Smestad <justin.smestad@gmail.com>
2013-09-30 16:43:58 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Socialite::Engine.routes.draw do
-  match '/signup', :to => 'users#new'
-  match '/auth/:provider/callback', :to => 'sessions#create'
+  match '/signup', :to => 'users#new', via: [:get]
+  match '/auth/:provider/callback', :to => 'sessions#create', via: [:post]
 
-  match '/logout', :to => 'sessions#destroy', :as => 'logout'
-  match '/login', :to => 'sessions#new', :as => 'login'
+  match '/logout', :to => 'sessions#destroy', :as => 'logout', via: [:all]
+  match '/login', :to => 'sessions#new', :as => 'login', via: [:get]
 
   resources :users # needed by omniauth-identity
 end
