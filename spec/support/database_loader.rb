c0d20b7d686abee294081db67e7a47de89da7a71
Justin Smestad <justin.smestad@gmail.com>
2013-10-14 11:51:21 -0700

    update travis config

--- spec/support/database_loader.rb
+++ spec/support/database_loader.rb
@@ -6,7 +6,7 @@ ActiveRecord::Base.establish_connection(db_name)
 ActiveRecord::Base.default_timezone = :utc
 
 load_schema = lambda {
-  # load File.join(File.dirname(__FILE__), "../dummy/db/schema.rb") # use db agnostic schema by default
+  load File.join(File.dirname(__FILE__), "../dummy/db/schema.rb") # use db agnostic schema by default
   ActiveRecord::Migrator.up(File.join(File.dirname(__FILE__), '../../db/migrate')) # use migrations
 }
 silence_stream(STDOUT, &load_schema)
